c3d71ca2f322b49d6798f5735c59fbf3565d210b
randriaa <randrianarizanany@gmail.com>
2021-10-17 14:21:59 +0200

    Messagerie Bidirectionnelle

--- client.py
+++ client.py
@@ -1,20 +1,33 @@
 ################# Coté Client ####################
-
+from threading import Thread
 import socket
 
 Host = "192.168.1.93"
 Port = 6390
 
+
+def Send(client):
+    while True:
+        msg = input("->")
+        msg = msg.encode('utf-8')
+        socket.send(msg)
+        
+def Reception(client): 
+    while True:
+        requete_server = socket.recv(500)
+        requete_server = requete_server.decode("utf-8")
+        print(requete_server)
+            
 #Création du socket
 socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 socket.connect((Host , Port))
 
-while True: 
-    msg = input("->")
-    msg = msg.encode('utf-8')
-    socket.send(msg)
+envoi = Thread(target=Send,args=[socket])
+recep = Thread(target=Reception,args=[socket])
+
+envoi.start()
+recep.start()  
     
-    requete_server = socket.recv(500)
-    requete_server = requete_server.decode("utf-8")
-    print(requete_server)
+    
+    
--- server.py
+++ server.py
@@ -1,7 +1,24 @@
 ################# Coté Serveur ####################
-
+from threading import Thread
 import socket
 
+
+def Send(client):
+    while True:
+        msg = input("->")
+        msg = msg.encode("utf-8")
+        client.send(msg)
+        
+def Reception(client): 
+    while True:
+        requete_client = client.recv(500)
+        requete_client = requete_client.decode('utf-8')
+        print(requete_client)
+        if not requete_client : #Si on perd la connexion
+            print("CLOSE")
+            break
+            
+            
 Host = "192.168.1.93"
 Port = 6390
 
@@ -16,16 +33,12 @@ client, ip = socket.accept()
 print("Le client d'adresse ip" , ip , "est connecté")
 
 
-while True: 
-    requete_client = client.recv(500)
-    requete_client = requete_client.decode('utf-8')
-    print(requete_client)
-    if not requete_client : #Si on perd la connexion
-        print("CLOSE")
-        break
-    msg = input("->")
-    msg = msg.encode("utf-8")
-    client.send(msg)
+envoi = Thread(target=Send,args=[client])
+recep = Thread(target=Reception,args=[client])
+
+envoi.start()
+recep.start()   
     
+recep.join() 
 client.close()
 socket.close()
